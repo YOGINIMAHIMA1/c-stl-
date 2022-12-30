f36933e9b762959d45e2dc1be3fb6d50d440d8bc
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-30 18:18:09 +0530

    Create pair-vector.cpp

---
+++ pair-vector.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ pair<int,int>p={1,3};
+ cout<<p.first<<" ,"<<p.second<<endl;
+ pair<pair<string,char>,pair<bool,int>>p1={{"standard",'s'},{0,9}};
+ cout<<p1.first.first<<" ,"<<p1.first.second<<", "<<p1.second.first<<" ,"<<p1.second.second<<endl;
+ }
